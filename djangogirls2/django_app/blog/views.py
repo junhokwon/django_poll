c931a11c02f38f9769dd62a78c39e14a6757c57b
junho Kwon <oilvers0904@gmail.com>
2017-05-31 01:24:56 +0900

    add post_create

--- djangogirls2/django_app/blog/views.py
+++ djangogirls2/django_app/blog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 from blog.models import Post
 
 
 def post_list(request):
-    post = Post.objects.all()
+    post = Post.objects.order_by('-created date')
     context = {
         'title' : '네이버 인기뉴스',
         'post' : post,
@@ -17,3 +20,22 @@ def post_detail(request,pk):
         'post' : post,
     }
     return render(request,'blog/post_detail.html', context=context)
+
+def post_create(request):
+    if request.method =='GET':
+        context = {
+
+        }
+        return render(request, 'blog/post_create.html',context=context)
+    elif request.method =='POST':
+        data = request.POST
+        title = data['title']
+        text = data['text']
+        user = User.objects.first()
+        post = Post.objects.create(
+            title = title,
+            text=text,
+            author=user,
+        )
+        #return HttpResponse(post)
+        return redirect('post_detail', pk=post.pk)
