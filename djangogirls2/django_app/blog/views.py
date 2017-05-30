355027d8773b0e99d998b99dd60f502055224391
junho Kwon <oilvers0904@gmail.com>
2017-05-31 01:10:09 +0900

    add post_detail.html,base.html, add url settings

--- djangogirls2/django_app/blog/views.py
+++ djangogirls2/django_app/blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 def post_list(request):
+    post = Post.objects.all()
     context = {
+        'title' : '네이버 인기뉴스',
+        'post' : post,
 
     }
     return render(request, 'blog/post_list.html',context=context)
+
+def post_detail(request,pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post' : post,
+    }
+    return render(request,'blog/post_detail.html', context=context)
