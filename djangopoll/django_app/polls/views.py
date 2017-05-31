a72ad8f8835ab7c8ae201c97641c83b82705ef9e
junho Kwon <oilvers0904@gmail.com>
2017-06-01 01:51:14 +0900

    add result.html/use generic_view(Listview,Detailview)

--- djangopoll/django_app/polls/views.py
+++ djangopoll/django_app/polls/views.py
@@ -1,26 +1,68 @@
+from audioop import reverse
+
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
-from django.shortcuts import render
+from django.views import generic
 
-from polls.models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list' : latest_question_list,
-    }
-    # return HttpResponse(template.render(request,context=context))
-    return render(request, 'polls/index.html',context=context)
+from polls.models import Question, Choice
 
 
-def detail(request, question_id):
-    return HttpResponse('너는 질문을 보고 있다 $s.' % question_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = '너는 질문의 결과를 보고 있다. %s.'
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list' : latest_question_list,
+#     }
+#     # return HttpResponse(template.render(request,context=context))
+#     return render(request, 'polls/index.html',context=context)
+#
+#
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise ('질문이 존재하지 않습니다')
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,'polls/detail.html',{'question' : question})
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context={
+#         'question' : question,
+#     }
+#     return render(request, 'polls/results.html',context=context)
+
 
 def vote(request, question_id):
-    return HttpResponse('너는 질문에 대한 투표를 하고 있다 $s.'% question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question' : question,
+            'error_message' : '너는 선택을 하지않았다.',
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
